096ae2a0951fd281c3f6ddd616a5e4c5bb7f4f91
KosmX <kosmx.mc@gmail.com>
2021-04-20 23:08:32 +0200

    olc-pge
    Using an useable game library

---
+++ game.cpp
@@ -0,0 +1,12 @@
+#define OLC_PGE_APPLICATION
+#include "olcPixelGameEngine.h"
+
+#include <iostream>
+#include <string>
+
+using namespace std;
+
+int main(int argc, char* argv[])
+{
+	cout << argv[0] << endl;
+}
